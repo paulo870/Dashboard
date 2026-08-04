1c7eea7fe5126a7838c2eadffcd4268080132827
paulo870 <paulo@poliglotaangola.com>
2026-08-04 15:42:45 +0100

    Update viewer.js

--- js/viewer.js
+++ js/viewer.js
@@ -1,16 +1,24 @@
+const params =
+new URLSearchParams(window.location.search);
 
-const params = new URLSearchParams(window.location.search);
 
-const BOOK_ID = params.get("id");
+const BOOK_ID =
+params.get("id");
 
-if (!BOOK_ID) {
 
-    alert("No book selected.");
+if(!BOOK_ID){
 
-    window.location.href = "index.html";
+alert("No book selected.");
+
+window.location.href="index.html";
 
 }
 
+
+
 const STORAGE =
-"https://BR-Intermediate.supabase.co/storage/v1/object/public/books/" +
-BOOK_ID + "/";
+"https://vjzcwbvyfnnwsuaxeauw.supabase.co/storage/v1/object/public/books/"
++
+BOOK_ID
++
+"/";
