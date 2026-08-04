8bc584d58f8e8d372d0aeaaabd7b694c262c7c4a
paulo870 <paulo@poliglotaangola.com>
2026-08-04 15:33:39 +0100

    Update books.js

--- js/books.js
+++ js/books.js
@@ -1,138 +1,19 @@
-// =====================================
-// POLIGLOTA DIGITAL LIBRARY
-// books.js
-// =====================================
-
+const STORAGE =
+"https://YOUR_PROJECT.supabase.co/storage/v1/object/public/books/";
 
 const books = [
 
+{
+    id:"BR-Intermediate",
 
-    // =====================================
-    // KIDS
-    // =====================================
+    title:"Business Result Intermediate",
 
+    category:"Business",
 
-    {
-        id: "kidsbox1",
+    password:"1234",
 
-        title: "Kids Box 1",
-
-        publisher: "Cambridge",
-
-        edition: "2nd Edition",
-
-        level: "Starter",
-
-        category: "Kids",
-
-        cover: "books/kidsbox1/cover.jpg",
-
-        path: "books/kidsbox1/index.html",
-
-        password: "1234"
-
-    },
-
-
-    {
-        id: "kidsbox2",
-
-        title: "Kids Box 2",
-
-        publisher: "Cambridge",
-
-        edition: "2nd Edition",
-
-        level: "Movers",
-
-        category: "Kids",
-
-        cover: "books/kidsbox2/cover.jpg",
-
-        path: "books/kidsbox2/index.html",
-
-        password: "1234"
-
-    },
-
-
-
-    // =====================================
-    // ADULTS
-    // =====================================
-
-
-    {
-        id: "headway-elementary",
-
-        title: "Headway Elementary",
-
-        publisher: "Oxford",
-
-        edition: "5th Edition",
-
-        level: "A2",
-
-        category: "Adults",
-
-        cover: "books/headway-elementary/cover.jpg",
-
-        path: "books/headway-elementary/index.html",
-
-        password: "1234"
-
-    },
-
-
-    {
-        id: "english-file-elementary",
-
-        title: "English File Elementary",
-
-        publisher: "Oxford",
-
-        edition: "4th Edition",
-
-        level: "A1-A2",
-
-        category: "Adults",
-
-        cover: "books/english-file-elementary/cover.jpg",
-
-        path: "books/english-file-elementary/index.html",
-
-        password: "1234"
-
-    },
-
-
-
-    // =====================================
-    // BUSINESS
-    // =====================================
-
-
-    {
-        id: "business-result",
-
-        title: "Business Result",
-
-        publisher: "Oxford",
-
-        edition: "Intermediate",
-
-        level: "B1",
-
-        category: "Business",
-
-        cover: "books/business-result/cover.jpg",
-
-        path: "books/business-result/index.html",
-
-        password: "1234"
-
-    }
-
-
+    cover:
+`${STORAGE}BR-Intermediate/images/homepage.jpeg`
+}
 
 ];
